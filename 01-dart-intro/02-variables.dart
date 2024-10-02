cd6934fdab74d4e6b7c730a06100f2332d6af019
WFriver <wrivera@quickapps.mx>
2024-10-02 13:26:10 -0500

    add dynamics variable
    to know how dynamics works

--- 01-dart-intro/02-variables.dart
+++ 01-dart-intro/02-variables.dart
@@ -6,12 +6,22 @@ void main () {
   final List<String> abilities = ['imopstor'];
   final sprites = <String>['ditto/front.png', 'ditto/back.png'];
   
+  // dynamic == null
+  //dynamic can has all the the values but this start with a null value
+  dynamic errorMessage = 'Hola';
+  errorMessage = true;
+  errorMessage = [1,2,3,4,5,6];
+  errorMessage = {1,2,3,4,5,6};
+  errorMessage = () => true;
+  errorMessage = null;
+  
   print("""
   $pokemon
   $hp
   $isAlive
   $abilities
   $sprites
+  $errorMessage
   
   """);
 }
