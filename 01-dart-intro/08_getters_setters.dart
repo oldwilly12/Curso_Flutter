da4929e7fee8e3cc3845647b9e748098798d29fc
oldwilly12 <rwilly203@gmail.com>
2024-10-03 22:34:33 -0600

    Update 08_getters_setters.dart

---
+++ 01-dart-intro/08_getters_setters.dart
@@ -0,0 +1,51 @@
+//PRIMER CONSTRUCTOR
+/*void main() {
+  
+  final mySquare = Square( side: 10);
+  
+  print ('area: ${mySquare.calculateArea()}');
+}
+
+class Square {
+  
+  double side; //side * side
+  
+  Square({ required this.side });
+  
+  double calculateArea(){
+    return side*side;
+  }
+}*/
+
+void main() {
+  
+  final mySquare = Square( side: 10);
+  
+  print ('area: ${mySquare.area}');
+}
+
+class Square {
+  
+ double _side; //side * side para convertir side en propiedad privada se escribiria de la siguiente manera _side
+  
+  Square({ required double side })
+    : _side = side;
+  
+  double get area {
+    return _side * _side;
+  }
+  
+  set side(double value){
+    print('setting new value $value');
+    if (value < 0) throw 'Value must be >=0';
+      
+    _side = value;
+    
+  }
+  
+  
+  
+  double calculateArea(){
+    return _side * _side;
+  }
+}
